709f19bb8cd2bd89cb43b09877a5849b4a1eb6bf
Valery <113440912+ValeriyVakulenko1445@users.noreply.github.com>
2025-01-30 20:23:36 +0300

    Добавление класса "CarRepYAML"_rep_yaml

--- car_repository.py
+++ car_repository.py
@@ -85,3 +85,55 @@ class CarRepJSON(CarRepBase):
 
     def get_count(self):
         return len(self.read_all())
+
+# Репозиторий YAML
+class CarRepYAML(CarRepBase):
+    def __init__(self, filename="cars.yaml"):
+        self.filename = filename
+        if not os.path.exists(filename):
+            with open(filename, "w") as f:
+                yaml.dump([], f)
+
+    def read_all(self):
+        with open(self.filename, "r") as f:
+            return yaml.safe_load(f) or []
+
+    def write_all(self, data):
+        with open(self.filename, "w") as f:
+            yaml.dump(data, f)
+
+    def get_by_id(self, car_id):
+        return next((car for car in self.read_all() if car["car_id"] == car_id), None)
+
+    def get_k_n_short_list(self, k, n):
+        data = self.read_all()
+        return data[n * k:(n + 1) * k]
+
+    def sort_by_field(self, field):
+        data = self.read_all()
+        data.sort(key=lambda x: x[field])
+        self.write_all(data)
+
+    def add_car(self, car):
+        data = self.read_all()
+        car.car_id = max([c["car_id"] for c in data], default=0) + 1
+        data.append(car.__dict__)
+        self.write_all(data)
+
+    def update_car(self, car_id, new_car):
+        data = self.read_all()
+        for i, car in enumerate(data):
+            if car["car_id"] == car_id:
+                data[i] = new_car.__dict__
+                self.write_all(data)
+                return True
+        return False
+
+    def delete_car(self, car_id):
+        data = self.read_all()
+        data = [car for car in data if car["car_id"] != car_id]
+        self.write_all(data)
+
+    def get_count(self):
+        return len(self.read_all())
+
